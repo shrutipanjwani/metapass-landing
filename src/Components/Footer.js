1091b87c1aa406ee3b752312f3e3e5c23c119186
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-04-08 16:20:20 +0530

    feat: :speech_balloon: more ui fixes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,55 +5,61 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiscord, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className="footer-container">
-        <div className="footer-col">
-          <div className="footer-logo-flex">
-            <img src={logo} alt="Logo" />
-            <p>&nbsp;Metapass</p>
-          </div>
-          <ul className="footer-socials">
-            <li>
-              <a href="/">
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-            </li>
-            <li>
-              <a href="/">
-                <FontAwesomeIcon icon={faDiscord} />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-col">
-          <ul className="footer-links">
-            <h3>Company</h3>
-            <li>
-              <a href="/">Twitter</a>
-            </li>
-            <li>
-              <a href="/">gm@metapasshq.xyz</a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-col">
-          <ul className="footer-links">
-            <h3>Legal</h3>
-            <li>
-              <a href="/">Terms of Service</a>
-            </li>
-            <li>
-              <a href="/">Privacy Policy</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="copyright">
-        <small>Copyright © 2022 Metapass, Inc. All rights reserved.</small>
-      </div>
-    </footer>
-  );
+	return (
+		<footer>
+			<center>
+				<hr className="separator" />
+			</center>
+			<div className="footer-container">
+				<div className="footer-col">
+					<div className="footer-logo-flex">
+						<img src={logo} alt="Logo" />
+					</div>
+					<ul className="footer-socials">
+						<li>
+							<a href="https://twitter.com/metapassHQ">
+								<FontAwesomeIcon icon={faTwitter} />
+							</a>
+						</li>
+						<li>
+							<a href="https://discord.gg/CBQ5YNnFXx">
+								<FontAwesomeIcon icon={faDiscord} />
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="footer-col">
+					<ul className="footer-links">
+						<h3>Company</h3>
+						<li>
+							<a href="https://twitter.com/metapassHQ">Twitter</a>
+						</li>
+						<li>
+							<a href="mailto:gm@metapasshq.xyz">
+								gm@metapasshq.xyz
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="footer-col">
+					<ul className="footer-links">
+						<h3>Legal</h3>
+						<li>
+							<a href="/">Terms of Service</a>
+						</li>
+						<li>
+							<a href="/">Privacy Policy</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="copyright">
+				<small>
+					Copyright © 2022 Metapass, Inc. All rights reserved.
+				</small>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -10,96 +10,102 @@ import ticket from "../Assets/ticket1.png";
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 const Main = () => {
-  return (
-    <main>
-      <div className="features-grid" data-aos="fade-up">
-        <div className="feature-image">
-          <img src={token} alt="token" />
-        </div>
-        <div className="feature-grid-content">
-          <h1>Token gate your events</h1>
-          <p>NFT Tickets to token gate your event in less than 1 minute</p>
-          <a href="/">
-            Learn more <FontAwesomeIcon icon={faArrowRight} />{" "}
-          </a>
-        </div>
-        <div className="feature-grid-content">
-          <h1>Live on Polygon Network</h1>
-          <p>
-            We've chosen to go live on Polygon chain as its fast and inexpensive
-            to create events
-          </p>
-          <a href="/">
-            Learn more <FontAwesomeIcon icon={faArrowRight} />{" "}
-          </a>
-        </div>
-        <div className="feature-image">
-          <img src={live} alt="live" />
-        </div>
-      </div>
-      <div className="features-method" data-aos="fade-up">
-        <h1>Fast and easy ticket verification</h1>
-        <p>Built with security and easy-of-use in mind.</p>
-        <div className="methods-flex">
-          <div className="method-image">
-            <img src={ticket} alt="ticket" />
-          </div>
-          <div className="method-content">
-            <a href="/">
-              <div className="method-content-box">
-                <FontAwesomeIcon icon={faDiscord} />
-                <p>Metapass Discord Bot for one click verification</p>
-              </div>
-            </a>
-            <br />
-            <a href="/">
-              <div className="method-content-box">
-                <FontAwesomeIcon icon={faQrcode} />
-                <p>One time QR code for IRL Events</p>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="pricing" data-aos="fade-up">
-          <h1>Pricing</h1>
-          <p>Completely free for creators</p>
-          <br />
-          <div className="pricing-box">
-            <h1>Free</h1>
-            <div className="price-flex">
-              <h2>$0</h2>
-              <span>/month</span>
-            </div>
-            <ul>
-              <li>Make Unlimited events</li>
-              <li>No limit on tickets</li>
-              <li>No commission</li>
-            </ul>
-            <GradientButton>
-              <a href="/">
-                Create an event&nbsp; <FontAwesomeIcon icon={faArrowRight} />{" "}
-              </a>
-            </GradientButton>
-          </div>
-        </div>
-        <div className="community" data-aos="fade-up">
-          <h1>Be a part of our awesome community!</h1>
-          <ul className="socials">
-            <li>
-              <a href="/">
-                <Button>Twitter</Button>
-              </a>
-            </li>
-            <li>
-              <a href="/">
-                <Button>Discord</Button>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="features-grid" data-aos="fade-up">
+				<div className="feature-image">
+					<img src={token} alt="token" />
+				</div>
+				<div className="feature-grid-content">
+					<h1>Token gate your events</h1>
+					<p>
+						NFT Tickets to token gate your event in less than 1
+						minute
+					</p>
+					<a href="/">
+						Learn more <FontAwesomeIcon icon={faArrowRight} />{" "}
+					</a>
+				</div>
+				<div className="feature-grid-content">
+					<h1>Live on Polygon Network</h1>
+					<p>
+						We've chosen to go live on Polygon chain as its fast and
+						inexpensive to create events
+					</p>
+					<a href="/">
+						Learn more <FontAwesomeIcon icon={faArrowRight} />{" "}
+					</a>
+				</div>
+				<div className="feature-image">
+					<img src={live} alt="live" />
+				</div>
+			</div>
+			<div className="features-method" data-aos="fade-up">
+				<h1>Fast and easy ticket verification</h1>
+				<p>Built with security and easy-of-use in mind.</p>
+				<div className="methods-flex">
+					<div className="method-image">
+						<img src={ticket} alt="ticket" />
+					</div>
+					<div className="method-content">
+						<a href="/">
+							<div className="method-content-box">
+								<FontAwesomeIcon icon={faDiscord} />
+								<p>
+									Metapass Discord Bot for one click
+									verification
+								</p>
+							</div>
+						</a>
+						<br />
+						<a href="/">
+							<div className="method-content-box">
+								<FontAwesomeIcon icon={faQrcode} />
+								<p>One time QR code for IRL Events</p>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div className="pricing" data-aos="fade-up">
+					<h1>Pricing</h1>
+					<p>Completely free for creators</p>
+					<br />
+					<div className="pricing-box">
+						<h1>Free</h1>
+						<div className="price-flex">
+							<span>$0/month</span>
+						</div>
+						<ul>
+							<li>Make Unlimited events</li>
+							<li>No limit on tickets</li>
+							<li>10% cut per ticket</li>
+						</ul>
+						<GradientButton>
+							<a href="https://app.metapasshq.xyz">
+								Create an event&nbsp;{" "}
+								<FontAwesomeIcon icon={faArrowRight} />{" "}
+							</a>
+						</GradientButton>
+					</div>
+				</div>
+				<div className="community" data-aos="fade-up">
+					<h1>Be a part of our awesome community!</h1>
+					<ul className="socials">
+						<li>
+							<a href="/">
+								<Button>Twitter</Button>
+							</a>
+						</li>
+						<li>
+							<a href="/">
+								<Button>Discord</Button>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Main;
